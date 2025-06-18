240871577fd0d12645c963740915998c0a0529ec
Jeffrey Sevinga <sevinga@protonmail.com>
2025-06-18 13:33:02 +0200

    Prepare composer package with inline repeater

--- src/Twill/Capsules/Redirects/Http/Controllers/RedirectController.php
+++ src/Twill/Capsules/Redirects/Http/Controllers/RedirectController.php
@@ -5,7 +5,8 @@ namespace TwillRedirects\Twill\Capsules\Redirects\Http\Controllers;
 use A17\Twill\Http\Controllers\Admin\SingletonModuleController as BaseModuleController;
 use A17\Twill\Services\Forms\Fields\Input;
 use A17\Twill\Services\Forms\Form;
-use A17\Twill\Services\Forms\Fields\Repeater;
+use A17\Twill\Services\Forms\InlineRepeater;
+use A17\Twill\Services\Forms\Fields\Select;
 use A17\Twill\Models\Contracts\TwillModelContract;
 
 class RedirectController extends BaseModuleController
@@ -21,7 +22,23 @@ class RedirectController extends BaseModuleController
         );
 
         $form->add(
-            Repeater::make()->name('redirects')->label('Redirects')
+            InlineRepeater::make()
+                ->name('redirects')
+                ->label('Redirects')
+                ->triggerText('Add redirect rule')
+                ->fields([
+                    Input::make()->name('from')->label('From'),
+                    Input::make()->name('to')->label('To'),
+                    Select::make()
+                        ->name('statuscode')
+                        ->label('Status code')
+                        ->options([
+                            ['value' => 301, 'label' => '301'],
+                            ['value' => 302, 'label' => '302'],
+                            ['value' => 307, 'label' => '307'],
+                            ['value' => 308, 'label' => '308'],
+                        ]),
+                ])
         );
 
         return $form;
