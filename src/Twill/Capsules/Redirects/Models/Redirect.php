4d48308528ea6213716801037115be0aad835807
Jeffrey Sevinga <sevinga@protonmail.com>
2025-06-19 10:26:01 +0200

    Flush redirect cache on model changes

--- src/Twill/Capsules/Redirects/Models/Redirect.php
+++ src/Twill/Capsules/Redirects/Models/Redirect.php
@@ -3,6 +3,7 @@
 namespace TwillRedirects\Twill\Capsules\Redirects\Models;
 
 use A17\Twill\Models\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Redirect extends Model
 {
@@ -15,4 +16,10 @@ class Redirect extends Model
     protected $casts = [
         'redirects' => 'array',
     ];
+
+    protected static function booted(): void
+    {
+        static::saved(fn () => Cache::forget('twill_redirects'));
+        static::deleted(fn () => Cache::forget('twill_redirects'));
+    }
 }
